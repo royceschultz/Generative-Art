56140a903a43341046df67a4f768e2dd36fad6f9
Royce <rosc1351@colorado.edu>
2019-08-18 14:41:08 -0600

    LeSs LiNeS dOeSn'T mAkE yOuR cOdE rUn FaStEr

--- pixelSort.py
+++ pixelSort.py
@@ -3,13 +3,6 @@ import matplotlib.image as mpimg
 import numpy as np
 import random
 
-def value(pixel):
-    return sum(pixel)
-
-def sortBySum(x):
-    sum = np.sum(x, axis=1)
-    return x[sum.argsort()]
-
 def dist(x,y):
     d = 0
     for i in range(len(x)):
@@ -23,20 +16,18 @@ def pixelSort(imageName, tol, numCycles):
     mpimg.imsave('SortImageFrames/'+str(0)+'.png',image,format='png',dpi=100)
 
     for cycle in range(numCycles):
-        print(cycle)
+        print('Frame '+str(cycle)+' of '+str(numCycles))
         for row in range(m):
             randCol = random.randint(0,n-1)
-            j = randCol
-            k = randCol
-
+            j, k = randCol, randCol
             while j > 1 and dist(image[row][j-1], image[row][randCol]) < tol:
                 j -= 1
             while k+1 < n and dist(image[row][k+1], image[row][randCol]) < tol and sum(image[row][k]) < 2:
                 k += 1
             if j != k:
-                image[row][j:k] = sortBySum(image[row][j:k])
+                x = image[row][j:k]
+                image[row][j:k] =  x[np.sum(x, axis=1).argsort()]
 
         mpimg.imsave('SortImageFrames/'+str(cycle+1)+'.png',image,format='png',dpi=100)
-
 
 pixelSort('image.png',0.42,100)
