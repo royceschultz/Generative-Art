81e921bd194be5cc9f29aab1752495e8279dafcd
Royce <rosc1351@colorado.edu>
2019-08-31 18:52:52 -0600

    Clusting exits on convergence instead of a defined number of cycles. Makes processes into functions to allow more versatility

--- PixelClustering/cluster.py
+++ PixelClustering/cluster.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import matplotlib
 import numpy as np
 
-im = Image.open('image.png')
+im = Image.open('image2.png')
 plt.imshow(im)
 
 im = np.array(im).astype('int')
@@ -15,7 +15,7 @@ def select_pixels(px,n):
     return px[sorted(np.random.randint(0,len(px),n))]
 
 def ham_score(px1, px2):
-    return sum(abs(px1-px2)**2)
+    return sum(abs(px1-px2)**2)**.5
 
 def frame(img,width=1): # Adds a white boarder
     m,n,o = img.shape
@@ -23,18 +23,18 @@ def frame(img,width=1): # Adds a white boarder
     canvas[width:-width,width:-width] = img
     return canvas.astype(int)
 
-def generate_image(filename):
-    numCycles = 100
+def generate_palette():
     numPixels = 12
     palette = select_pixels(pixels,12).astype(float) # Randomly select starting pixels
-
-    for i in range(numCycles):
+    pdist = 0
+    max_dist = 9999
+    while max_dist > 55:
         choice = select_pixels(pixels,100*numPixels) # Select training data
         direction = np.zeros((numPixels,4)) # Initialize cost vector
         counts = np.ones(numPixels) # Record counts for normalization
         max_dist = 0 # Record max distance to monitor convergence
         for j,x in enumerate(choice): # For each training point
-            min_dist = 999
+            min_dist = 9999
             min_idx = -1
             for k,y in enumerate(palette): # For each palette point
                 dist = ham_score(x,y)
@@ -48,7 +48,12 @@ def generate_image(filename):
 
         counts = np.tile(counts,(4,1)).transpose() # reshape for division
         direction = np.divide(direction,counts) # Normalize
-        palette += direction.max()*0.001*direction # Train
+        palette += max_dist*0.001*direction # Train
+        print('dist',max_dist, end='\r')
+    print('d')
+    return palette
+
+def save_frame(palette, filename):
     canvas = frame(palette.reshape(4,3,4))
     cw,ch,cd = canvas.shape
     ow,oh,od = 600,500,4
@@ -60,6 +65,9 @@ def generate_image(filename):
     plt.imshow(output.astype('uint8'))
     matplotlib.image.imsave(filename,output.astype('uint8'))
 
-for i in range(50):
+for i in range(3):
     print(i)
-    generate_image('output'+str(i+1)+'.png')
+    palette = generate_palette()
+    for j in range(50):
+        np.random.shuffle(palette)
+        save_frame(palette,'output/'+str(i+1)+'-'+str(j+1)+'.png')
